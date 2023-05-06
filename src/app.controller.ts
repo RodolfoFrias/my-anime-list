f6473af2522f7bfcd7513444e0cca2402c9baed1
Rodolfo Frías <rodofriaslugo@gmail.com>
2023-05-06 14:54:37 -0600

    feat: set cache manually

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,40 @@
-import { Controller, Get, Res, Next, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Res, Next, Inject } from '@nestjs/common';
 import { Response, NextFunction } from 'express';
 import { AppService } from './app.service';
-import { CacheInterceptor } from '@nestjs/cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 @Controller()
-@UseInterceptors(CacheInterceptor)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private TTL = 1000;
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Get('/animes')
   async allAnimes(@Res() res: Response, @Next() next: NextFunction) {
     try {
-      const allAnimes = await this.appService.getAnimes();
-      res.json(allAnimes);
+      let allAnimes;
+      const isCacheExpired = await this.isExpired();
+
+      if (isCacheExpired) {
+        allAnimes = await this.appService.getAnimes();
+        await this.setCache(allAnimes);
+      }
+
+      const animesToReturn = await this.cacheManager.get('animes');
+
+      res.json(animesToReturn);
     } catch (error) {
       next(error);
     }
+  }
+
+  private async setCache(value): Promise<void> {
+    await this.cacheManager.set('animes', value, this.TTL);
+  }
+
+  private isExpired(): Promise<boolean> {
+    return Promise.resolve(true);
   }
 }
